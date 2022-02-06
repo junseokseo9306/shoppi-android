e100ee957445dffded586ca38c2d32b0be3b5150
Injin Bae <ivy.gobiz@gmail.com>
2022-02-07 00:47:43 +0900

    [섹션9] 카테고리 목록 API에 데이터 요청 구현

---
+++ app/src/main/java/com/shoppi/app/network/ApiClient.kt
@@ -0,0 +1,37 @@
+package com.shoppi.app.network
+
+import com.shoppi.app.model.Category
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+interface ApiClient {
+
+    @GET("categories.json")
+    suspend fun getCategories(): List<Category>
+
+    companion object {
+
+        private const val baseUrl = "Please enter your url"
+
+        fun create(): ApiClient {
+
+            val logger = HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BASIC
+            }
+
+            val client = OkHttpClient.Builder()
+                .addInterceptor(logger)
+                .build()
+
+            return Retrofit.Builder()
+                .baseUrl(baseUrl)
+                .client(client)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+                .create(ApiClient::class.java)
+        }
+    }
+}
---
+++ app/src/main/java/com/shoppi/app/repository/category/CategoryRepository.kt
@@ -0,0 +1,12 @@
+package com.shoppi.app.repository.category
+
+import com.shoppi.app.model.Category
+
+class CategoryRepository(
+    private val remoteDataSource: CategoryRemoteDataSource
+) {
+
+    suspend fun getCategories(): List<Category> {
+        return remoteDataSource.getCategories()
+    }
+}
