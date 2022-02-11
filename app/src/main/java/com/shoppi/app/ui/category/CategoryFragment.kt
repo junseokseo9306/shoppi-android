846833203a0b61d49d35e0d244f7c61bbde77fdb
Injin Bae <ivy.gobiz@gmail.com>
2022-02-11 22:53:21 +0900

    [섹션10] 카테고리 선택 이벤트 발생시 세부 카테고리 화면으로 이동 구현

--- app/src/main/java/com/shoppi/app/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/category/CategoryFragment.kt
@@ -4,9 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.shoppi.app.R
+import com.shoppi.app.common.KEY_CATEGORY_ID
+import com.shoppi.app.common.KEY_CATEGORY_LABEL
 import com.shoppi.app.databinding.FragmentCategoryBinding
+import com.shoppi.app.ui.common.EventObserver
 import com.shoppi.app.ui.common.ViewModelFactory
 
 class CategoryFragment: Fragment() {
@@ -26,10 +32,29 @@ class CategoryFragment: Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val categoryAdapter = CategoryAdapter()
+        binding.lifecycleOwner = viewLifecycleOwner
+        setListAdapter()
+        setNavigation()
+    }
+
+    private fun setListAdapter() {
+        val categoryAdapter = CategoryAdapter(viewModel)
         binding.rvCategoryList.adapter = categoryAdapter
         viewModel.items.observe(viewLifecycleOwner) {
             categoryAdapter.submitList(it)
         }
+    }
+
+    private fun setNavigation() {
+        viewModel.openCategoryEvent.observe(viewLifecycleOwner, EventObserver {
+            openCategoryDetail(it.categoryId, it.label)
+        })
+    }
+
+    private fun openCategoryDetail(categoryId: String, categoryLabel: String) {
+        findNavController().navigate(R.id.action_category_to_category_detail, bundleOf(
+            KEY_CATEGORY_ID to categoryId,
+            KEY_CATEGORY_LABEL to categoryLabel
+        ))
     }
 }
