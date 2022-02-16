dc900ab82e2e43fd0989f7eb3502f60c9a7d0332
Injin Bae <ivy.gobiz@gmail.com>
2022-02-17 04:29:17 +0900

    [섹션11] 과제 - 홈화면 배너 디자인 적용 및 상품상세화면으로 이동 구현

--- app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/home/HomeFragment.kt
@@ -4,11 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.google.android.material.tabs.TabLayoutMediator
 import com.shoppi.app.R
+import com.shoppi.app.common.KEY_PRODUCT_ID
 import com.shoppi.app.databinding.FragmentHomeBinding
+import com.shoppi.app.ui.common.EventObserver
 import com.shoppi.app.ui.common.ViewModelFactory
 
 class HomeFragment : Fragment() {
@@ -30,6 +34,7 @@ class HomeFragment : Fragment() {
 
         binding.lifecycleOwner = viewLifecycleOwner
         setToolbar()
+        setNavigation()
         setTopBanners()
     }
 
@@ -39,9 +44,17 @@ class HomeFragment : Fragment() {
         })
     }
 
+    private fun setNavigation() {
+        viewModel.openProductEvent.observe(viewLifecycleOwner, EventObserver { productId ->
+            findNavController().navigate(R.id.action_home_to_product_detail, bundleOf(
+                KEY_PRODUCT_ID to productId
+            ))
+        })
+    }
+
     private fun setTopBanners() {
         with(binding.viewpagerHomeBanner) {
-            adapter = HomeBannerAdapter().apply {
+            adapter = HomeBannerAdapter(viewModel).apply {
                 viewModel.topBanners.observe(viewLifecycleOwner, { banners ->
                     submitList(banners)
                 })
